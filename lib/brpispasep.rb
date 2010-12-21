adccea43cc05655e3e7f6d983cd30c52c1fda5da
Rodrigo Manhães <rmanhaes@gmail.com>
2010-12-21 19:01:43 -0200

    generated error is associated to the pis_pasep field

--- lib/brpispasep.rb
+++ lib/brpispasep.rb
@@ -1,11 +1,9 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-%w(pis_pasep).each {|req| require File.dirname(__FILE__) + "/brpispasep/#{req}"}
+%w(pis_pasep pis_pasep_active_record).each {|req| require File.dirname(__FILE__) + "/brpispasep/#{req}"}
 
 %w(rubygems active_record active_support).each {|req| require req }
-
-#ActiveRecord::Base.send :include, CpfCnpjActiveRecord
 
 module BrPisPasep
 end
